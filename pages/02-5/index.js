77d52f4af7a5c82e5fae0f4ad86f5b26db48acbe
hotte00 <junpio0812@kaist.ac.kr>
2025-01-12 20:41:32 +0900

    Section 05 과제 완료

--- pages/02-5/index.js
+++ pages/02-5/index.js
@@ -9,7 +9,14 @@ import {
     Email,
     Password,
     LogInButton,
-} from '../../styles/02-5'
+    MenuWrapper,
+    SignUp,
+    SearchEmail,
+    SearchPassword,
+    MenuDivider,
+    KakaoLogInButton,
+    KakaoIcon,
+} from '../../styles/02-5.js'
 
 export default function ItsroadSignUpPage(){
         // javascript Zone
@@ -17,15 +24,28 @@ export default function ItsroadSignUpPage(){
     return(
         // html Zone
         <Wrapper>
+            <KakaoLogInButton>
+                <KakaoIcon src={'img/KakaoIcon.png'}></KakaoIcon>
+                카카오톡으로 로그인
+            </KakaoLogInButton>
+            
+            <MenuWrapper>
+                <SearchEmail>이메일찾기</SearchEmail>
+                <MenuDivider></MenuDivider>
+                <SearchPassword>비밀번호찾기</SearchPassword>
+                <MenuDivider></MenuDivider>
+                <SignUp>회원가입</SignUp>
+            </MenuWrapper>
+
             <SignUpWrapper>
                 <Email type='text' placeholder='이메일을 입력해주세요'></Email>
                 <Password type='password' placeholder='비밀번호를 입력해주세요'></Password>
+                <LogInButton>로그인</LogInButton>
             </SignUpWrapper>
 
             <TitleWrapper>
                 <TitleImage src={`img/titleimage.png`}></TitleImage>
                 <TitleImage2 src={`img/titleimage2.png`}></TitleImage2>
-                <LogInButton></LogInButton>
                 <Title>잇츠로드</Title>
             </TitleWrapper>
         </Wrapper>
--- styles/02-5.js
+++ styles/02-5.js
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled"
+import { KnownArgumentNamesOnDirectivesRule } from "graphql/validation/rules/KnownArgumentNamesRule"
 
 
 export const Wrapper = styled.div`
     margin-left: 80px;
     margin-top: 50px;
     width: 640px;
+    height: 1138px;
     border: 1px solid red;
     background-image: url('/img/02-5-background.png');
     display: flex;
@@ -15,6 +17,7 @@ export const Wrapper = styled.div`
 
 export const TitleWrapper = styled.div`
     width: 540px;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -25,6 +28,7 @@ export const TitleImage = styled.img`
     width: 67px;
     height: 82px;
     // background-color: white;
+    margin-bottom: -10px;
 `
 
 export const TitleImage2 = styled.img`
@@ -37,6 +41,7 @@ export const Title = styled.div`
     font-size: 60px;
     font-family: Arial, Helvetica, sans-serif;
     color: white;
+    margin-top: 40px;
 `
 
 // export const Temp = styled.div`
@@ -50,11 +55,90 @@ export const SignUpWrapper = styled.div`
 
 export const Email = styled.input`
     width: 100%;
+    height: 40px;
+    border: none;
     border-bottom: 1px solid #7D7D7D;
-    margin-bottom: 20px;
+    background-color: transparent;
+    font-size: 24px;
+    margin-bottom: 40px;
+    color: white;
 `
 
 export const Password = styled.input`
     width: 100%;
+    height: 40px;
+    border: none;
     border-bottom: 1px solid #7D7D7D;
+    background-color: transparent;
+    font-size: 24px;
+    color: white;
+    margin-bottom: 60px;
+`
+
+export const LogInButton = styled.button`
+    width: 100%;
+    height: 76px;
+    background-color: rgba(255, 27, 109, 0.5);
+    font-size: 26px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+    color: white;
+    border-radius: 38px;
+    border: none;
+`
+
+export const MenuWrapper = styled.div`
+    width: 540px;
+    display: flex;
+    flex-direction: row;
+    // align-items: center;
+    justify-content: space-around;
+    margin-top: 50px;
+`
+
+export const SignUp = styled.div`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+`
+
+export const MenuDivider = styled.div`
+    border-right: 2px solid #9F9F9F;
+`
+
+export const SearchEmail = styled.div`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+`
+
+export const SearchPassword = styled.div`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+`
+
+export const KakaoLogInButton = styled.button`
+    width: 540px;
+    height: 76px;
+    border-radius: 38px;
+    border-color: #FAE100;
+    background: none;
+    font-size: 26px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+    color: #FAE100;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 85px;
+    margin-top: 70px;
+`
+
+export const KakaoIcon = styled.img`
+    padding-right: 40px;
 `
